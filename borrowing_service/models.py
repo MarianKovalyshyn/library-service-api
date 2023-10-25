180e6b6ddcecc727eae66156cc1f4635fb6b4127
Andrii Kurta <andriykurta97@gmail,com>
2023-10-25 17:08:14 +0200

    Change user and book fields and change test_models.py file

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -1,20 +1,25 @@
+from django.conf import settings
 from django.db import models
+
+from book_service.models import Book
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateTimeField()
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField()
-    book_id = models.IntegerField()
-    user_id = models.IntegerField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
 
     class Meta:
         ordering = ["-borrow_date"]
 
     def __str__(self):
         return (
-            f"User_id: {self.user_id}\n" 
-            f"Book id: {self.book_id}\n"
+            f"User: {self.user.email}\n" 
+            f"Book: {self.book.title}\n"
             f"Borrow date: {self.borrow_date},\n"
             f"Expected return date: {self.expected_return_date}\n"
             f"Actual return date: {self.actual_return_date}"
--- borrowing_service/tests/test_models.py
+++ borrowing_service/tests/test_models.py
@@ -1,28 +1,44 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils import timezone
 
+from book_service.models import Book
 from borrowing_service.models import Borrowing
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
 
 
 class TestBorrowingModel(TestCase):
     def test_borrowing_str_method(self):
+        payload = {
+            "email": "tes1t@test.com",
+            "password": "testpass",
+        }
         borrow_date = timezone.now()
         expected_return_date = borrow_date + timezone.timedelta(days=14)
         actual_return_date = borrow_date + timezone.timedelta(days=10)
-        book_id = 1
-        user_id = 1
+        book = Book.objects.create(
+            title="test_title",
+            author="test_author",
+            daily_fee=20.50,
+            cover="SOFT",
+            inventory=5
+        )
+        user = create_user(**payload)
 
         borrowing = Borrowing.objects.create(
             borrow_date=borrow_date,
             expected_return_date=expected_return_date,
             actual_return_date=actual_return_date,
-            book_id=book_id,
-            user_id=user_id,
+            book=book,
+            user=user,
         )
 
         expected_str = (
-            f"User_id: {user_id}\n"
-            f"Book id: {book_id}\n"
+            f"User: {user.email}\n"
+            f"Book: {book.title}\n"
             f"Borrow date: {borrow_date},\n"
             f"Expected return date: {expected_return_date}\n"
             f"Actual return date: {actual_return_date}"
