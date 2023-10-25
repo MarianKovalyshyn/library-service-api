6ab392bbf6369231fe8aa41c92cf7e26e87b7ad5
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-25 12:43:15 +0300

    Add model Book to models.py Create test_models.py

---
+++ book_service/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from book_service.models import Book
+
+
+class BookTests(TestCase):
+    def test_car_str(self):
+        book = Book.objects.create(
+            title="test_title",
+            author="test_author",
+            daily_fee=20,
+            cover="SOFT",
+            inventory=5
+        )
+        self.assertEqual(
+            str(book),
+            f"Title:{book.title};"
+            f" Author: {book.author};"
+            f" Daily fee: {book.daily_fee}")
