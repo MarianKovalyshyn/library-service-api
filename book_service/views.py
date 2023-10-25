34d265a3ce80ffff80ce1c625dc9c04c4277221d
Illia <kononenko2109@gmail.com>
2023-10-25 15:59:06 +0300

    removed permissions

--- book_service/views.py
+++ book_service/views.py
@@ -2,10 +2,8 @@ from rest_framework import viewsets
 
 from book_service.models import Book
 from book_service.serializers import BookSerializer
-from borrowing_service.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
