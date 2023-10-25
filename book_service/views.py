bcfdba618b8597f24a3fc040141ac37b5c1bbed4
Illia <kononenko2109@gmail.com>
2023-10-25 15:51:35 +0300

    uncommented imports

--- book_service/views.py
+++ book_service/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 
 from book_service.models import Book
-# from book_service.serializers import BookSerializer
-# from borrowing_service.permissions import IsAdminOrIfAuthenticatedReadOnly
+from book_service.serializers import BookSerializer
+from borrowing_service.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
