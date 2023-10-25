a8e5d83bcae233dd66577f18be1989ed579c922b
Andrii Kurta <andriykurta97@gmail,com>
2023-10-25 13:00:41 +0200

    Add borrowing model to models.py file in borrowing_service app

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField()
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField()
+    book_id = models.IntegerField()
+    user_id: models.IntegerField()
+
+    class Meta:
+        ordering = ["-borrow_date"]
+
+    def __str__(self):
+        return (
+            f"User_id: {self.user_id}\n" 
+            f"Book id: {self.book_id}\n"
+            f"Borrow date: {self.borrow_date},\n"
+            f"Expected return date: {self.expected_return_date}\n"
+            f"Actual return date: {self.actual_return_date}"
+        )
