382f44139225ec732360a0757802c509f6096579
anna-lybid <anna.lybid@gmail.com>
2023-10-25 13:36:18 +0300

    serializers for book_service + tests, commented serializer for borrowing model

--- borrowing_service/serializers.py
+++ borrowing_service/serializers.py
@@ -1,16 +1,16 @@
-from rest_framework import serializers
-
-from .models import Borrowing
-
-
-class BorrowingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrowing
-        fields = (
-            "id",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-            "book",
-            "user",
-        )
+# from rest_framework import serializers
+#
+# from .models import Borrowing
+#
+#
+# class BorrowingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Borrowing
+#         fields = (
+#             "id",
+#             "borrow_date",
+#             "expected_return_date",
+#             "actual_return_date",
+#             "book",
+#             "user",
+#         )
---
+++ tests/test_serializers.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookSerializerTest(TestCase):
+    def setUp(self):
+        self.book_data = {
+            "title": "Test Book",
+            "author": "Author Test",
+            "cover": "COVER",
+            "inventory": 2,
+            "daily_fee": 5.55,
+        }
+        self.book = Book.objects.create(**self.book_data)
+
+    def test_serializer_output(self):
+        serializer = BookSerializer(instance=self.book)
+        expected_data = {
+            "title": "Test Book",
+            "author": "Author Test",
+            "cover": "COVER",
+            "inventory": 2,
+            "daily_fee": 5.55,
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_valid_serializer(self):
+        serializer = BookSerializer(data=self.book_data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_invalid_serializer(self):
+        payload = {
+            "title": "Invalid Book",
+            "cover": "Author Test",
+            "inventory": 2,
+            "daily_fee": 5.55,
+        }
+        serializer = BookSerializer(data=payload)
+        self.assertFalse(serializer.is_valid())
