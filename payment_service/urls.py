671889daa95367f9eed10447f03d23966163dfd8
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-26 13:47:32 +0300

    Add views for views.py

---
+++ payment_service/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import PaymentListView, PaymentDetailView
+
+urlpatterns = [
+    path("payments/", PaymentListView.as_view(), name="payment-list"),
+    path("payments/<int:pk>/", PaymentDetailView.as_view(), name="payment-detail"),
+]
+
+app_name = "payment-service"
--- payment_service/views.py
+++ payment_service/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Payment
+from .serializers import PaymentListSerializer, PaymentDetailSerializer
 
-# Create your views here.
+
+class PaymentListView(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(user=self.request.user)
+
+
+class PaymentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentDetailSerializer
+    permission_classes = [IsAuthenticated]
