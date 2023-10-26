22febdd0e6c4ac385412df521f1e7b71c0fab5a4
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-26 14:06:08 +0300

    Add more serializers and tests

--- payment_service/serializers.py
+++ payment_service/serializers.py
@@ -1,17 +1,14 @@
 from rest_framework import serializers
-
 from payment_service.models import Payment
 
 
-class PaymentSerializer(serializers.ModelSerializer):
+class PaymentListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = (
-            "id",
-            "status",
-            "type",
-            "borrowing_id",
-            "session_url",
-            "session_id",
-            "money_to_pay",
-        )
+        fields = '__all__'
+
+
+class PaymentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
--- payment_service/tests/test_serializers.py
+++ payment_service/tests/test_serializers.py
@@ -1,13 +1,11 @@
 from django.test import TestCase
-
 from payment_service.models import Payment
-from payment_service.serializers import PaymentSerializer
+from payment_service.serializers import PaymentListSerializer, PaymentDetailSerializer
 
 
 class PaymentSerializerTest(TestCase):
     def setUp(self):
         self.payment_data = {
-            "id": 1,
             "status": "PENDING",
             "type": "FINE",
             "borrowing_id": 3,
@@ -17,10 +15,10 @@ class PaymentSerializerTest(TestCase):
         }
         self.payment = Payment.objects.create(**self.payment_data)
 
-    def test_serializer_output(self):
-        serializer = PaymentSerializer(instance=self.payment)
+    def test_payment_list_serializer(self):
+        serializer = PaymentListSerializer(instance=self.payment)
         expected_data = {
-            "id": 1,
+            "id": self.payment.id,
             "status": "PENDING",
             "type": "FINE",
             "borrowing_id": 3,
@@ -30,15 +28,27 @@ class PaymentSerializerTest(TestCase):
         }
         self.assertEqual(serializer.data, expected_data)
 
-    def test_invalid_serializer(self):
-        payload = {
-            "id": 1,
+    def test_payment_detail_serializer(self):
+        serializer = PaymentDetailSerializer(instance=self.payment)
+        expected_data = {
+            "id": self.payment.id,
             "status": "PENDING",
             "type": "FINE",
             "borrowing_id": 3,
             "session_url": "https://test-url/",
             "session_id": "2",
-            "money_to_pay": 300000.12,
+            "money_to_pay": "300000.12",
         }
-        serializer = PaymentSerializer(data=payload)
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_create_invalid_data(self):
+        invalid_data = {
+            "status": "INVALID_STATUS",
+            "type": "INVALID_TYPE",
+            "borrowing_id": -1,
+            "session_url": "invalid-url",
+            "session_id": "",
+            "money_to_pay": "invalid_amount",
+        }
+        serializer = PaymentListSerializer(data=invalid_data)
         self.assertFalse(serializer.is_valid())
