80100d3e21c8c76eb5c2cf4fff372444864f45cf
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-26 13:07:57 +0300

    Add views to payment-service/views.py

---
+++ payment_service/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from payment_service.views import PaymentViewSet
+
+router = routers.DefaultRouter()
+
+router.register("payments", PaymentViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "payment-service"
--- payment_service/views.py
+++ payment_service/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payment_service.models import Payment
+from payment_service.serializers import (
+    PaymentListSerializer,
+    PaymentDetailSerializer,
+    PaymentSerializer
+)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class PaymentListView(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(user=self.request.user)
+
+
+class PaymentDetailView(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentDetailSerializer
+    permission_classes = [IsAuthenticated]
