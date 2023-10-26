dd42206c891603daca34e89416b4ebb9869f933a
Yaroslav Mazurkevych <77452871+yaroslav-mazurkevych@users.noreply.github.com>
2023-10-26 14:28:44 +0300

    Update views.py

--- payment_service/views.py
+++ payment_service/views.py
@@ -1,22 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from payment_service.models import Payment
-from payment_service.serializers import PaymentListSerializer, PaymentDetailSerializer
+from payment_service.serializers import PaymentSerializer
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
-    serializer_class = PaymentListSerializer
+    serializer_class = PaymentSerializer
 
     def get_queryset(self):
         if self.request.user.is_staff:
             return Payment.objects.all()
         return Payment.objects.filter(user=self.request.user)
-
-    def get_serializer_class(self):
-        if self.action == "retrieve" or self.action == "update":
-            return PaymentDetailSerializer
-        return PaymentListSerializer
 
     def get_permissions(self):
         if self.action in ["list", "create"]:
