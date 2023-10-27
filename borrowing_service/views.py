4ed3b002cadab40b78ffbcc24db83ad2afd88529
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-27 16:23:19 +0300

    add fine payment

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -16,6 +16,7 @@ from borrowing_service.serializers import (
     BorrowingReturnSerializer,
     BorrowingCreateSerializer,
 )
+from payment_service.models import Payment
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -111,14 +112,29 @@ class BorrowingViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
+        borrowing.actual_return_date = timezone.now()
+
+        days = (borrowing.actual_return_date - borrowing.borrow_date).days
+        amount = days * borrowing.daily_fee
+        existing_payment = Payment.objects.get(borrowing_id=borrowing.id, type="FINE")
+        existing_payment.money_to_pay = amount
+        existing_payment.save()
+
+        if borrowing.actual_return_date > borrowing.expected_return_date:
+            days_overdue = (borrowing.expected_return_date - borrowing.actual_return_date).days
+            fine_amount = days_overdue * borrowing.daily_fee * 2
+
+            existing_payment = Payment.objects.get(borrowing_id=borrowing.id, type="FINE")
+            existing_payment.money_to_pay += fine_amount
+            existing_payment.save()
+
+        borrowing.save()
+
         book = borrowing.book
         book.inventory += 1
         book.save()
 
-        borrowing.actual_return_date = timezone.now()
-        borrowing.save()
-
         return Response(
-            {"message": "Borrowing returned successfully"},
+            {"message": f"Borrowing returned successfully"},
             status=status.HTTP_200_OK
         )
